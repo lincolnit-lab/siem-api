df417b13c6589f6d97fd7bc21bc6471309c7d226
lincoln.it-lab <lincoln.it@quantum-projects.info>
2026-04-14 10:34:28 +0000

    Auth 1

--- app/db/models.py
+++ app/db/models.py
@@ -2,6 +2,12 @@ from sqlalchemy import Column, Integer, String, DateTime
 from datetime import datetime
 from app.db.database import Base
 
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+
 class Ban(Base):
     __tablename__ = "bans"
 
